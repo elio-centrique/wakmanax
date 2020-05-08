eee16ff1eb5b4ba2b8cde2531c97534ef9a56aa4
Elio-Centrique <alan.delannel@ynov.com>
2020-05-09 00:25:49 +0200

    test

--- wakmanax.js
+++ wakmanax.js
@@ -74,36 +74,34 @@ i18next.init({
 
 function send_message() {
     fetch('http://almanax.kasswat.com', {method: 'get'}).then(res => res.json()).then((json) => {
-        if(!almanax_sent) { 
-            let embed;
-            console.log(client.guilds.cache)
-            client.guilds.cache.forEach(guild => {
-                console.log("test")
-                collection.findOne({guild: {$eq: guild.name}}, (err, cursor) => {
-                    console.log("test2")
-                    if(cursor.language == 'fr' || cursor.language == 'français' || cursor.language == 'french') {
-                        embed = new Discord.MessageEmbed().setTitle(json['day'] + " " + json['month'] + " " + json['year'])
-                        .setDescription(json['description'][0])
-                        .addField('bonus', json['bonus'][0])
-                        .setImage('https://vertylo.github.io/wakassets/merydes/' + json['img'] + '.png')
-                    } else {
-                        embed = new Discord.MessageEmbed().setTitle(json['day'] + " " + json['month'] + " " + json['year'])
-                        .setDescription(json['description'][1])
-                        .addField('bonus', json['bonus'][1])
-                        .setImage('https://vertylo.github.io/wakassets/merydes/' + json['img'] + '.png')
+        console.log(client.guilds.cache)
+        let embed;
+        client.guilds.cache.forEach(guild => {
+            console.log("test")
+            collection.findOne({guild: {$eq: guild.name}}, (err, cursor) => {
+                console.log("test2")
+                if(cursor.language == 'fr' || cursor.language == 'français' || cursor.language == 'french') {
+                    embed = new Discord.MessageEmbed().setTitle(json['day'] + " " + json['month'] + " " + json['year'])
+                    .setDescription(json['description'][0])
+                    .addField('bonus', json['bonus'][0])
+                    .setImage('https://vertylo.github.io/wakassets/merydes/' + json['img'] + '.png')
+                } else {
+                    embed = new Discord.MessageEmbed().setTitle(json['day'] + " " + json['month'] + " " + json['year'])
+                    .setDescription(json['description'][1])
+                    .addField('bonus', json['bonus'][1])
+                    .setImage('https://vertylo.github.io/wakassets/merydes/' + json['img'] + '.png')
+                }
+                console.log(client.channels.cache.get(cursor.channel))
+                if(client.channels.cache.get(cursor.channel)) {
+                    try {
+                        client.channels.cache.get(cursor.channel).send(embed)
+                    } catch(error) {
+                        console.log(cursor.guild + ": Please update the Bot Permissions.");
                     }
-                    console.log(client.channels.cache.get(cursor.channel))
-                    if(client.channels.cache.get(cursor.channel)) {
-                        try {
-                            client.channels.cache.get(cursor.channel).send(embed)
-                        } catch(error) {
-                            console.log(cursor.guild + ": Please update the Bot Permissions.");
-                        }
-                    }
-                })
-            });
-            console.log(i18next.t("senteveryone"))
-        }
+                }
+            })
+        });
+        console.log(i18next.t("senteveryone"))
     })
 }
 
@@ -127,7 +125,7 @@ function setLanguage(message, language = undefined) {
 
 client.once('ready', () => {
     try {
-        cron.schedule('24 23 * * *', () =>{
+        cron.schedule('27 23 * * *', () =>{
             console.log('sending almanax from cron');
             send_message();
             almanax_sent = true;
